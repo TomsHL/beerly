000fae07c4628afd2fd68677062e203c85283656
cosme deMargon <cocosme@gmail.com>
2021-11-24 12:40:57 +0100

    Comment added to predictor class

--- beerly/predictor.py
+++ beerly/predictor.py
@@ -9,7 +9,9 @@ from beerly.params import PATH, RATE
 class Predictor(object):
     def __init__(self, user, type_rate, beer_from_ocr):
         """
-            user: pandas Series
+            user: TO BE DEFINED
+            type_rate : str : ratings to be used for pred / possible value : ['palate', 'overall', 'appearance', 'aroma', 'taste']
+            beer_from_ocr = list : obtained from the OCR_Read class
         """
         self.user = user
         self.type_rate = type_rate
@@ -17,7 +19,10 @@ class Predictor(object):
 
 
     def load_model(self, type_rate):
-        """Save the model into a file in surprise format"""
+        """
+            Load the already trained rating model from a surprise file
+            type_rate : str : ratings to be used for pred / possible value : ['palate', 'overall', 'appearance', 'aroma', 'taste']
+        """
 
         #add mean to select correct model from rating param
         #FILE_NAME = RATE['rating']
@@ -27,12 +32,21 @@ class Predictor(object):
         return model
 
     def evaluate(self, user, model):
-        """evaluates the pipeline on df_test and return the RMSE"""
+        """
+            Make a prediction for a user based on the chosen rating and return the prediction for ALL beers
+            user : TO BE DEFINED
+            model :  result from load_model() function
+        """
         y_pred = model.predict(user)
         return y_pred
 
     def filter_result(y_pred, beer_from_ocr):
-        #
+        """
+            Filter the predictions from the chosen model by the of beer coming from OCR
+            y_pred : result from evaluate() function
+            beer_from_ocr = list : obtained from the OCR_Read class
+        """
+        
         return filtered_list_of_beer
 
 if __name__ == "__main__":
