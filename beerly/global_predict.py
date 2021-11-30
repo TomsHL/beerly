307cc1b46e0d6d76f97a1ba167ce9fd1770b1aa5
cosme deMargon <cocosme@gmail.com>
2021-11-30 17:22:48 +0100

    modified global predict

--- beerly/global_predict.py
+++ beerly/global_predict.py
@@ -16,19 +16,18 @@ def global_pred(
 
     beer_ranking = pd.merge(result_collab,result_content,on='beer_id')
 
-    temp = pd.Series(data=np.zeros((len(mixing_parameters))), dtype="float32")
+    beer_ranking["ranking"] = (beer_ranking.score * 10 * mixing_parameters['content'] +
+                               beer_ranking.rating_aroma * mixing_parameters['aroma'] +
+                               beer_ranking.rating_taste * mixing_parameters['taste'] +
+                               beer_ranking.rating_palate * mixing_parameters['palate'] +
+                               beer_ranking.rating_overall * mixing_parameters['overall'] +
+                               beer_ranking.rating_appearance * mixing_parameters['appearance']
+                              )/ sum(mixing_parameters.values())
 
-    for atrib in Atrib:
-        temp = beer_ranking[f"rating_{atrib.val()}"]* mixing_parameters[atrib.val()] + temp
-        beer_ranking.drop(columns=[f"rating_{atrib.val()}"], inplace=True)
-
-    beer_ranking["ranking"] = (temp + beer_ranking.score *10* mixing_parameters['content'])/ sum(mixing_parameters.values())
-    beer_ranking.drop(columns='score',inplace=True)
+    beer_ranking = beer_ranking[['beer_id','beer_name','ranking']]
     beer_ranking.sort_values(
         by=["ranking"], ascending=False, ignore_index=True, inplace=True
     )
-
-    return beer_ranking
 
 if __name__ == "__main__":
     pass
